5533671bb185c165cebf0efd4493a499c5ee71bd
Atharv <atharvbhandare2001@gmail.com>
2021-12-30 18:38:06 +0530

    Interfaces

--- public/app.js
+++ public/app.js
@@ -1,22 +1,18 @@
-import { Invoice } from './classes/Invoice.js';
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-// invOne.client = 'yoshi';
-// invOne.amount = 50;
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach(inv => {
-    console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
-});
-const form = document.querySelector('.new-item-form');
-console.log(form.children);
-// inputs
-const type = document.querySelector('#type');
-const tofrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
-});
+"use strict";
+const me = {
+    name: 'Andrew',
+    age: 20,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+/* also we can pass argument inside any function but that argument should be of the type we declared as interface */
+const greetSpiderman = (spiderman) => {
+    console.log('hello ', spiderman.name);
+};
+greetSpiderman(me);
+console.log(me);
--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,28 @@
-import {Invoice} from './classes/Invoice.js'
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+// Interface
+interface IsSpiderman {
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
 
-// invOne.client = 'yoshi';
-// invOne.amount = 50;
+const me: IsSpiderman = {
+    name: 'Andrew',
+    age: 20,
+    speak(text: string): void {
+        console.log(text);
+    },
+    spend(amount: number): number {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
 
-let invoices: Invoice[] = [];
-invoices.push(invOne)
-invoices.push(invTwo);
+/* also we can pass argument inside any function but that argument should be of the type we declared as interface */
+const greetSpiderman = (spiderman: IsSpiderman) => {
+    console.log('hello ', spiderman.name)
+}
 
-invoices.forEach(inv => {
-  console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
-})
+greetSpiderman(me);
+console.log(me);
 
-
-
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
-
-// inputs
-const type = document.querySelector('#type') as HTMLInputElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
-
-form.addEventListener('submit', (e: Event) => {
-  e.preventDefault();
-
-  console.log(
-    type.value, 
-    tofrom.value, 
-    details.value, 
-    amount.valueAsNumber
-  );
-});
