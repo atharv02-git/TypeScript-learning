19884e7056292b6c9f6011875ceed49ad16ed5f5
Atharv <atharvbhandare2001@gmail.com>
2021-12-30 18:02:29 +0530

    modules

--- public/app.js
+++ public/app.js
@@ -1,34 +1,22 @@
-"use strict";
-class Invoice {
-    // readonly client: string; /* Read-only members can be accessed outside the class, but their value cannot be changed. Since read-only members cannot be changed outside the class, they either need to be initialized at declaration or initialized inside the class constructor. */
-    // private details: string; /* private access modifier ensures that class members are visible only to that class and are not accessible outside the containing class */
-    // public amount: number; /* by default, all members of a class in TypeScript are public. All the public members can be accessed anywhere without any restrictions. */
-    // constructor(c: string, d: string, a: number) {
-    //     this.client = c;
-    //     this.details = d;
-    //     this.amount = a;
-    // } 
-    /* alternative for modifiers to be used inside the constructor*/
-    constructor(
-    /* note: it's not gonna assign our variables if we remove modifiers inside the constructor */
-    client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
-// creating a new object based on the instance of the object of class Invoice
-const invOne = new Invoice('mario', 'work on the mario website', 200);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 600);
-// console.log(invOne, invOne)
-/* what if in the future we want to add only the object that has Invoice type inside an array */
+import { Invoice } from './classes/Invoice.js';
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+// invOne.client = 'yoshi';
+// invOne.amount = 50;
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-// console.log(invoices);
 invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());
+    console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
+});
+const form = document.querySelector('.new-item-form');
+console.log(form.children);
+// inputs
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,36 @@
-class Invoice {
-    // readonly client: string; /* Read-only members can be accessed outside the class, but their value cannot be changed. Since read-only members cannot be changed outside the class, they either need to be initialized at declaration or initialized inside the class constructor. */
+import {Invoice} from './classes/Invoice.js'
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
-    // private details: string; /* private access modifier ensures that class members are visible only to that class and are not accessible outside the containing class */
+// invOne.client = 'yoshi';
+// invOne.amount = 50;
 
-    // public amount: number; /* by default, all members of a class in TypeScript are public. All the public members can be accessed anywhere without any restrictions. */
-
-    // constructor(c: string, d: string, a: number) {
-    //     this.client = c;
-    //     this.details = d;
-    //     this.amount = a;
-    // } 
-    
-    /* alternative for modifiers to be used inside the constructor*/
-
-    constructor(
-        /* note: it's not gonna assign our variables if we remove modifiers inside the constructor */
-        readonly client: string,
-        private details: string,
-        public amount: number
-    ){}
-
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`
-    }
-}
-// creating a new object based on the instance of the object of class Invoice
-const invOne = new Invoice('mario', 'work on the mario website', 200);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 600);
-// console.log(invOne, invOne)
- /* what if in the future we want to add only the object that has Invoice type inside an array */
 let invoices: Invoice[] = [];
-invoices.push(invOne);
+invoices.push(invOne)
 invoices.push(invTwo);
-// console.log(invoices);
+
 invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format())
-}) 
+  console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
+})
+
+
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
+
+// inputs
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  console.log(
+    type.value, 
+    tofrom.value, 
+    details.value, 
+    amount.valueAsNumber
+  );
+});
