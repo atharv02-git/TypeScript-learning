eb06588cba1290273c7d3e5f8607c0f22adb5cd2
Atharv <atharvbhandare2001@gmail.com>
2021-12-30 15:48:33 +0530

    working with classes

--- public/app.js
+++ public/app.js
@@ -1,4 +1,23 @@
 "use strict";
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+// creating a new object based on the instance of the object of class Invoice
+const invOne = new Invoice('mario', 'work on the mario website', 200);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 600);
+// console.log(invOne, invOne)
+/* what if in the future we want to add only the object that has Invoice type inside an array */
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
 // const anchor = document.querySelector('a');
 // if (anchor) {
 //     console.log(anchor)
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,28 @@
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d: string, a: number) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`
+    }
+}
+// creating a new object based on the instance of the object of class Invoice
+const invOne = new Invoice('mario', 'work on the mario website', 200);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 600);
+// console.log(invOne, invOne)
+ /* what if in the future we want to add only the object that has Invoice type inside an array */
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
+
 // const anchor = document.querySelector('a');
 // if (anchor) {
 //     console.log(anchor)
@@ -12,7 +37,7 @@ const form = document.querySelector('.new-item-form') as HTMLFormElement;
 const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
-const amount= document.querySelector('#amount') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
 
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
