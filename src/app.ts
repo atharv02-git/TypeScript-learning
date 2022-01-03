f6ee44b7d2207ab12bb95c92bf973ef14a6d3b5e
Atharv <atharvbhandare2001@gmail.com>
2022-01-03 16:04:11 +0530

    project done

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,29 @@
 import { Invoice } from './classes/Invoice.js'
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payment.js'
 import { HasFormatter } from './interfaces/HasFormatter'
 
-// deciding the type of the document
-let docOne: HasFormatter;
-let docTwo: HasFormatter;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
-docOne = new Invoice('Atharv', 'project building', 200);
-docTwo = new Payment('Andrew', 'plumbing', 180);
+// inputs
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
 
-// also we can decide the type of array
-let docArr: HasFormatter[] = [];
-docArr.push(docOne, docTwo);
-console.log(docArr)
+// List Template instance
+const ul = document.querySelector('ul')!;
+const list  = new ListTemplate(ul);
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+
+    let doc: HasFormatter;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+    } else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+    }
+
+    list.render(doc, type.value, 'start');
+});
